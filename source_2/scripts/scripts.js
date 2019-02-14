4ef70857ae483847927754fceac2906c4f6ebed1
Granier_Thomas <thomas-granier@outlook.fr>
2019-02-14 11:45:12 +0100

    fin

--- source_2/scripts/scripts.js
+++ source_2/scripts/scripts.js
@@ -2,10 +2,18 @@ var shift = 0;
 var interval = 10; //ms
 var mySquare;
 var val;
-var ref=1000;
-var E = [0,0]; //[n-2,n-1]
-var S = [0,0]; //[n-2,n-1]
-Kp=5;
+var ref = 1000;
+var E = [0, 0]; //[n-2,n-1]
+var S = [0, 0]; //[n-2,n-1]
+var Kp = 10;
+var Ki = 1;
+var Kd = 1;
+var Erreur = 0;
+var Total_Erreur = 0;
+var Erreur_Prec = 0;
+var P = 0;
+var I = 0;
+var D = 0;
 
 function init() {
     mySquare = document.getElementById("square");
@@ -13,16 +21,22 @@ function init() {
 }
 
 function move_square() {
-    val.style.width=ref+"px";
-    val.innerHTML="ref = "+ref+" Kp = "+Kp;
-    shift = 0.002586*E[1]+0.0016858*E[0]+1.5113*S[1]-0.5487610*S[0];
-    E[0]=E[1];
-    command=(ref-shift)*Kp;
-    E[1]=command
-    S[0]=S[1];
-    S[1]=shift;
+    val.style.width = (ref + 60) + "px";
+    val.innerHTML = "ref = " + ref + " Kp = " + Kp;
+    shift = 0.002586 * E[1] + 0.0016858 * E[0] + 1.5113 * S[1] - 0.5487610 * S[0];
+    E[0] = E[1];
+    Erreur = ref - shift;
+    Total_Erreur += Erreur;
+    P = Erreur * Kp
+    I = Total_Erreur * Ki;
+    D = (Erreur - Erreur_Prec) * Kd;
+    Erreur = Erreur_Prec;
+    command = P + I + D;
+    E[1] = command
+    S[0] = S[1];
+    S[1] = shift;
     mySquare.style.left = shift + "px";
-    mySquare.innerHTML=shift;
+    mySquare.innerHTML = shift;
 }
 
 setInterval("move_square()", interval);
